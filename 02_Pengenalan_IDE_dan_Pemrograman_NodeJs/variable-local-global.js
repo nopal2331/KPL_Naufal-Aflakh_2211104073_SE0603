376b6f57473f81af3f7b7a62bfa51655a49acfd4
nopal2331 <naufalwijayanto3@gmail.com>
2025-03-21 05:13:47 +0700

    praktikum 2

---
+++ 02_Pengenalan_IDE_dan_Pemrograman_NodeJs/variable-local-global.js
@@ -0,0 +1,12 @@
+let globalVarible = "Naufal Aflakh Wijayanto"
+
+function getMyIdentity() {
+    let localVariable = 22111004073
+
+    console.log('Nama saya ${globalVarible} nim ${localVariable}')
+}
+
+console.log(globalVarible)
+// console.log(localVariable)
+
+getMyIdentity()
---
+++ 02_Pengenalan_IDE_dan_Pemrograman_NodeJs/variable.js
@@ -0,0 +1,15 @@
+var name = "Naufal Aflakh Wijayanto"
+const nim = 2211104073
+
+console.log(name)
+
+console.log("nama saya adalah " + name + " dan nim saya adalah " + nim)
+console.log('Nama saya adalah ${name} dan nim saya adalah ${nim}')
+
+console.log(`Nama saya adalah ${name}
+    nim saya adalah ${nim}`)
+
+
+var name = "ayam"
+
+let fullName = "Naufal Aflakh Wijayanto"
